b5df734e8450e39547b6f9dd945f02f173c5f7a3
Francisco Aranda <fran.vs.2003@gmail.com>
2023-10-26 17:35:46 -0300

    Added two functions isCellOccupied y getCellFree

--- include/game.h
+++ include/game.h
@@ -3,7 +3,14 @@
 
 typedef struct rep_board * TBoard;
 
-// Funcion para crear un nuevo tablero 4x4;
+// Funcion para crear un nuevo tablero 4x4
 TBoard createNewTBoard();
+
+// Check if it cell is free
+// Retorna true si esta ocupada, false si no
+bool isCellOccupied(TBoard board, int f, int c);
+
+// Get an random free poscition on the board
+void getCellFree(TBoard board, int * f, int *c);
 
 #endif
--- src/game.cpp
+++ src/game.cpp
@@ -1,22 +1,41 @@
 #include "../include/raylib.h"
 #include "../include/game.h"
+#include <time.h>
+#include <stdlib.h>
+
+#define size 4
 
 struct rep_board {
+    int score;
     int ** cell;
 };
 
 TBoard createNewTBoard() {
     TBoard nuevo = new rep_board;
-    nuevo->cell = new int*[4]; // Primero asignamos memoria para las filas
-    for (int i = 0; i < 4; i++) {
-        nuevo->cell[i] = new int[4]; // Luego asignamos memoria para las columnas de cada fila
+    nuevo->cell = new int*[size]; // Primero asignamos memoria para las filas
+    for (int i = 0; i < size; i++) {
+        nuevo->cell[i] = new int[size]; // Luego asignamos memoria para las columnas de cada fila
     }
 
     // Inicializamos todas las celdas a 0
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 4; j++) {
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
             nuevo->cell[i][j] = 0;
         }
     }
     return nuevo;
+}
+
+bool isCellOccupied(TBoard board, int f, int c) {
+    if (board->cell[f][c] == 0)
+        return false;
+    return true;
+}
+
+void getCellFree(TBoard board, int * f, int *c) {
+    srand(time(NULL));
+    do {
+        *f = rand() % size;
+        *c = rand() % size;
+    } while(isCellOccupied(board, *f, *c));
 }
